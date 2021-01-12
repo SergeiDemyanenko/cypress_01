c11851f9c4c2730d3de30df856eaf3561e6775a7
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2021-01-11 19:38:43 -0800

    test PR

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -4,7 +4,7 @@ describe('My First Test', () => {
 
     cy.contains('type').click()
 
-    // Should be on a new URL which includes '/commands/actions'
+    // Should be on a new URL which includes /commands/actions
     cy.url().should('include', '/commands/actions')
 
     // Get an input, type into it and verify that the value has been updated
